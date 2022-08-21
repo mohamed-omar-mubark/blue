081b669acc18ef174782225c1236c3d8ce09e760
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-08-21 21:43:46 +0200

    chore: add vue-i18n

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>This is the home page</p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-}
+  export default {
+    name: 'Home'
+  }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false,
+      includeLocales: false,
+      enableBridge: true
+    }
+  }
 })
